578f572c65a5373d1f76768a061cdb6cffd885b5
pnoonan32 <patrick.a.noonan1@gmail.com>
2018-08-11 00:08:23 -0400

    Updated 8/11/2018 (12:08am) Updated bugs and added new feature

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -12,6 +12,18 @@ for ( i in gifs ) {
 
 });
 
+var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=sacramentokings&api_key=dc6zaTOxFJmzC&limit=1");
+xhr.done(function(response) {
+    console.log("success got data", response);
+
+var sacramentoKingsLogo = response.data
+
+for ( i in sacramentoKingsLogo) {
+    $("#kingsLogo").prepend("<a href='https://www.nba.com/kings/' target='_blank'><img style='width:300px;height:300px;' id='kingsGif' class='GiphyImages text-center mx-auto' src='" +sacramentoKingsLogo[i].images.original.url+"'</a>")
+}
+
+});
+
 
 
 
